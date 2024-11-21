753a96961a28d8063cf0a0cf0956f9a071df59ca
Rahitya Velpuri <33275106+rahitya-123@users.noreply.github.com>
2024-11-20 19:46:09 -0500

    country names standardization

---
+++ standardising-country-names.R
@@ -0,0 +1,47 @@
+library(sf)
+library(readxl)
+library(leaflet)
+library(countrycode)
+library(tidyverse)
+
+# ==== Import ====
+
+world_sf <- read_sf("data/world-shapefiles")
+
+country_population <- read_excel("data/country-gdp.xlsx")
+
+country_population <- country_population %>%
+  mutate(iso_a3=countrycode(country_name,
+                            "country.name",
+                            "iso3c"))
+
+# ==== Join ====
+
+world_gdp <- world_sf %>% 
+  left_join(country_population,
+            by = c("iso_a3"= "iso_a3"))
+
+
+
+# ==== Standardise country names ====
+
+
+countrycode("Commonwealth of Australia",
+            "country.name",
+            "iso3c")
+
+
+
+# ==== Data visualisation Code ====
+
+pal_gdp <- colorNumeric("viridis", world_gdp$gdp_md_est)
+
+world_gdp %>% 
+  leaflet() %>% 
+  addPolygons(fillColor = ~pal_gdp(gdp_md_est),
+              color = "white",
+              fillOpacity = 1,
+              weight = 1) %>% 
+  addLegend(pal = pal_gdp,
+            values = ~gdp_md_est,
+            opacity = 1)
